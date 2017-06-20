3e450ab2476c0325e708395947ed2ac82d447bcf
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2017-06-19 22:50:22 -0700

    updated state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react'
 
 //define a new class called SearchBar and enable it with all functionality of react.cmponent class
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'starting value' };
+  }
+
   render() {
-    return <input onChange={(event) => console.log(event.target.value)} />;
+    return (
+    <div>
+      <input
+        value = {this.state.term}
+        onChange={event => this.setState({ term: event.target.value })} />
+    </div>
+    );
   }
 }
-
+//state is a plain javascript object that is used to record and react to events
+//only class based components have state. functional components do not have state
 export default SearchBar;
