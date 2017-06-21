6afcc6708b15f4e46aaa93115c9257c613b30b71
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2017-06-21 06:46:25 -0700

    updated and tested video search function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
-
-const API_KEY = 'AIzaSyB4L0tAMPhMNvE4cuVlrziTVNhrjzIpXAA';
+const API_KEY = 'AIzaSyBi463TfHx9e9W6yQFByst6_q-8IJCA6D0';
 
 // components are snippets of code that produce html
-
 // create a new component
-const App = () => {
-  //const or constant is immutable
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'seafood'}, (videos) => {
+      this.setState({ videos }); //when key and value are the same
+    });
+
+  }
+
+  render() {
   return (
     <div>
       <SearchBar />
     </div>
-  );
+    );
+  }
 }
 
 // be sure the component gets inserted into the DOM so the user can see it
