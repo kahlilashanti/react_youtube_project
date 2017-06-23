fea03fc4af086479f6b2f2dc1cb7a9aa84ec6f76
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2017-06-23 16:34:19 -0700

    updated search functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -17,19 +18,24 @@ class App extends Component {
       selectedVideo: null
      };
 
-    YTSearch({key: API_KEY, term: 'seafood'}, (videos) => {
+     this.videoSearch('seafood');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: 'term'}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
        }); //when key and value are the same
     });
-
   }
 
   render() {
+    const videoSearch =_.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
